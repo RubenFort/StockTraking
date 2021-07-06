0ec4dcc1ec5fef174403e2c3c584cf04868dfdfb
Ruben <rdfortu@gmail.com>
2021-07-06 15:59:34 +0200

    Conexion DB y estructura del proyecto

---
+++ StockTracking/BLL/IBLL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTracking.BLL
+{
+    //T => xxxDetailDTO
+    //K => xxxDTO
+    interface IBLL<T, K> where T : class where K : class
+    {
+        K Select();
+        bool Insert(T entity);
+        bool Update(T entity);
+        bool Delete(T entity);
+        bool GetBack(int Id);
+    }
+}
---
+++ StockTracking/DAL/DAO/IDAO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTracking.DAL.DAO
+{
+    //T => Tipo de dato, modelo, clase del modelo
+    //K => Clase que vamos a utilizar, clase del detalle del modelo
+    interface IDAO<T, K> where T: class where K: class
+    {
+        List<K> Select();
+        bool Insert(T entity);
+        bool Update(T entity);
+        bool Delete(T entity);
+        bool GetBack(int Id);
+    }
+}
