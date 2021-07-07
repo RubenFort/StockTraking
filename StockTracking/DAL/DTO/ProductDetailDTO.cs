82b9b1a59968f0633855de374dd3bba1294cdb77
Ruben <rdfortu@gmail.com>
2021-07-07 18:54:12 +0200

    Añadir y listar products => Recupero categories para mostrar en ComboBox

---
+++ StockTracking/DAL/DTO/ProductDetailDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTracking.DAL.DTO
+{
+    public class ProductDetailDTO
+    {
+        public string productName { get; set; }
+        public string categoryName { get; set; }
+        public int stockAmount { get; set; }
+        public int price { get; set; }
+        public int productId { get; set; }
+        public int categoryId { get; set; }
+    }
+}
