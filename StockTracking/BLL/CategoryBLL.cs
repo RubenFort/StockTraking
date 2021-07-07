a07ecb777c91b1fbdee3bec51ac2e0af451abfcb
Ruben <rdfortu@gmail.com>
2021-07-07 03:08:23 +0200

    Añadir y listar categorias => Creo estructura

---
+++ StockTracking/BLL/CategoryBLL.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StockTracking.BLL;
+using StockTracking.DAL.DTO;
+
+namespace StockTracking.BLL
+{
+    public class CategoryBLL : IBLL<CategoryDetailDTO, CategoryDTO>
+    {
+        public bool Delete(CategoryDetailDTO entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool GetBack(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(CategoryDetailDTO entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CategoryDTO Select()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(CategoryDetailDTO entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StockTracking/DAL/DAO/CategoryDAO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StockTracking.DAL.DTO;
+
+namespace StockTracking.DAL.DAO
+{
+    public class CategoryDAO : IDAO<CATEGORY, CategoryDetailDTO>
+    {
+        public bool Delete(CATEGORY entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool GetBack(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(CATEGORY entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CategoryDetailDTO> Select()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(CATEGORY entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
